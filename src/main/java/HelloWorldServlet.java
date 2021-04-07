32076ea00b4e0cd0e56d86ffa7cde3a4fd7964d3
Ann Michelle Gonzales <ann.michelle.gonzales112@gmail.com>
2021-04-07 17:29:48 -0500

    servlet set up

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -1,9 +1,21 @@
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class HelloWorldServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest req, HttpServletResponse res) {
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
 
+@WebServlet(name = "HelloWorldServlet", urlPatterns = "/hello-world")
+public class HelloWorldServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<h2>Hello, World!</h2>");
     }
+
+
 }
