4ec7f3a54dcaba0f12e44b3696042531c4cffec8
Ann Michelle Gonzales <ann.michelle.gonzales112@gmail.com>
2021-04-12 14:50:26 -0500

    refactored view color

--- src/main/java/ViewColorServlet.java
+++ src/main/java/ViewColorServlet.java
@@ -17,8 +17,8 @@ public class ViewColorServlet {
 //    @Override
 //    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-    }
 }
+
 
 
 
